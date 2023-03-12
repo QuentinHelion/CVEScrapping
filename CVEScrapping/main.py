b711ce17d52783a4997e3545768f870eea72f13f
QuentinHelion <helion.quentin@gmail.com>
2023-03-12 12:09:04 +0100

    debug search

--- CVEScrapping/main.py
+++ CVEScrapping/main.py
@@ -1,13 +1,15 @@
-from Class import *
 from Interfaces import *
 #CVE-2022-23943
 
 def main():
     print("Starting...")
-    
+
     stdscr = curses.initscr()
     mainMenu(stdscr)
-    
+
+    # result = CVEScraper()
+    # print(result.search("apache2"))
+
     print("Loading...")
 
     print("ending...")
